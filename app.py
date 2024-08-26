0f5511704bcdadc6fab8359ad7dc4c16e93fddaf
Deepak Kumar <krdeepu777@gmail.com>
2024-08-26 11:46:17 +0530

    chages

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ def predict(stock_symbol):
 
     return prediction, current_price, predicted_price
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)  # Or use 0.0.0.0 for network access
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
 
